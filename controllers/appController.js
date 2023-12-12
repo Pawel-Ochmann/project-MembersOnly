cb8db8230ca7853f8e720c02416e7d8500349e2c
Pawel Ochmann <pawelochmann@poczta.onet.pl>
2023-12-12 14:35:33 +0100

    Working on sing up post request validator (especially custiom validators)

--- controllers/appController.js
+++ controllers/appController.js
@@ -1,5 +1,5 @@
 const User = require('../models/user');
-const { body, validatorResult } = require('express-validator');
+const { body, validationResult } = require('express-validator');
 const asyncHandler = require('express-async-handler');
 
 exports.main_page = asyncHandler(async (req, res, next) => {
@@ -20,14 +20,41 @@ exports.sign_up_post = [
     .trim()
     .isLength({ min: 1 })
     .escape()
-    .withMessage('You have to write Your second name'),
+    .withMessage('You have to write Your second name')
+    .escape(),
+  body('email')
+    .trim()
+    .isEmail()
+    .withMessage('This is not a valid email address'),
+  body('password')
+    .trim()
+    .isLength({ min: 8 })
+    .withMessage('Must contain at least 8 characters')
+    .isLowercase()
+    .withMessage('Password must contain at least one lowercase')
+    .isUppercase()
+    .withMessage('Password must contain at least one uppercase')
+    .isNumeric()
+    .withMessage('Password must contain at least one number')
+    .escape(),
+  body('password2'),
+  // custom validators
+  body('email').custom(async (value, { req, res, next }) => {
+    const user = await User.find({ email: value });
+    if (user) res.render('sign_up', { errors: ['E-mail is already in use'] });
+  }),
+  body('password2').custom(async (value, { req, res, next }) => {
+    const firstPassword = req.body.password;
+    if (firstPassword !== value)
+      res.render('sign_up', { errors: ['Passwords are not the same'] });
+  }),
   asyncHandler(async (req, res, next) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
       // Handle validation errors
       // You may want to customize this based on your needs
-      res.render('/sing-up', {
+      res.render('sign_up', {
         errors: errors.array(),
       });
       return;
